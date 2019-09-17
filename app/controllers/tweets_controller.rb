3eee35242d0835395358dac8fada0cf6b898b663
EC2 Default User <ec2-user@ip-172-31-33-25.ap-northeast-1.compute.internal>
2019-09-17 08:14:18 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,44 @@
+class TweetsController < ApplicationController
+def index
+ @tweets= Tweet.all
+ @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+end
+
+def show
+ @tweet = Tweet.find(params[:id])
+end
+
+def new
+ @tweet = Tweet.new
+end
+
+def create
+ @tweet = Tweet.create(tweet_params)
+ redirect_to tweets_path
+end
+
+def edit
+ @tweet = Tweet.find(params[:id])
+end
+
+def update
+ @tweet = Tweet.find(params[:id])
+ if @tweet.update(params.require(:tweet).permit(:name, :text))
+  redirect_to tweets_path
+ else
+  render"edit"
+ end
+end
+
+def destroy
+ @tweet=Tweet.find(params[:id])
+ @tweet.destroy
+ redirect_to tweets_path
+end 
+
+private
+def tweet_params
+    params.permit(:name,:text)
+end
+
+end
